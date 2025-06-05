7b622f99d28960641537e5058652ba2ab4d17f7b
rajnishad24082003 <rajnishad24082003@gmail.com>
2025-06-05 21:21:11 +0530

    readme file added

--- routes/route.js
+++ routes/route.js
@@ -4,12 +4,16 @@ const auth = require("../contollers/auth");
 const router = express.Router();
 
 router.get("/", controls.homePage);
-router.post("/books", auth.verifySession, controls.postBook);
 router.get("/books", controls.getAllBooks);
 router.post("/signup", auth.signup);
 router.post("/login", auth.login);
+
+router.post("/books", auth.verifySession, controls.postBook);
 router.get("/books/:id", auth.verifySession, controls.getSingleBook);
+
+//this paths are modified for optimization
 router.post("/books/:id/reviews", auth.verifySession, controls.postReview);
+router.put("/books/:id/reviews", auth.verifySession, controls.postReview);
 router.delete("/books/:id/reviews", auth.verifySession, controls.deleteReview);
 
 module.exports = router;
